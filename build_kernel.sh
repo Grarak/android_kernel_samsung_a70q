d2194ac4be723f1f626396451e77b5accb7e83a3
Willi Ye <williye97@gmail.com>
2019-07-25 01:53:19 +0200

    configs: Disable samsung protection features

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,7 +17,7 @@ KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 #make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android sm6150_sec_defconfig
 #make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
 
-make -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a70q_eur_open_defconfig
+make -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a70q_defconfig
 make -j16 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
 tools/mkdtimg create out/dtbo.img --page_size=4096 $(find out -name "*.dtbo")
